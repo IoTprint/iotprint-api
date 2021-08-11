7a7f5e9bbd4e61262dc7f82f416adfa05d94a8c8
ahmedaidev <52345388+ahmedaidev@users.noreply.github.com>
2021-08-11 23:20:18 +0200

    FIX(app): Database URI

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# iotprint-server
+# iotprint-api
 
 ![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
 ![Prerequisite](https://img.shields.io/badge/node-14.4.0-blue.svg)
--- app.js
+++ app.js
@@ -12,55 +12,60 @@ const productRoutes = require('./routes/product')
 const jobRoutes = require('./routes/job')
 const desktopRoutes = require('./routes/desktop')
 
-const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@iotprint-cluster.iknlv.mongodb.net/${process.env.DEFAULT_DATABASE}?retryWrites=true&w=majority`
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@node-cluster.znfsd.mongodb.net/${process.env.DEFAULT_DATABASE}?retryWrites=true&w=majority`
 
 const app = express()
 
 const fileStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		if (file.mimetype.split('/')[0] === 'image') {
-			cb(null, 'images')
-		} else {
-			cb(null, 'uploads')
-		}
-	},
-	filename: (req, file, cb) => {
-		cb(
-			null,
-			`${crypto.randomBytes(8).toString('hex')}.${file.originalname.split('.')[1]}`
-		)
-	},
+  destination: (req, file, cb) => {
+    if (file.mimetype.split('/')[0] === 'image') {
+      cb(null, 'images')
+    } else {
+      cb(null, 'uploads')
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${crypto.randomBytes(8).toString('hex')}.${
+        file.originalname.split('.')[1]
+      }`
+    )
+  },
 })
 
 const fileFilter = (req, file, cb) => {
-	console.log(file.mimetype)
-	if (
-		file.mimetype === 'image/png' ||
-		file.mimetype === 'image/jpg' ||
-		file.mimetype === 'image/jpeg' ||
-		// file.mimetype === 'model/stl' ||
-		file.mimetype === 'application/octet-stream'
-	) {
-		cb(null, true)
-	} else {
-		cb(null, false)
-	}
+  console.log(file.mimetype)
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg' ||
+    // file.mimetype === 'model/stl' ||
+    file.mimetype === 'application/octet-stream'
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
 }
 
 app.use(bodyParser.json())
 app.use(
-	multer({ storage: fileStorage, fileFilter }).fields([
-		{ name: 'image', maxCount: 1 },
-		{ name: 'gcode', maxCount: 1 },
-	])
+  multer({ storage: fileStorage, fileFilter }).fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'gcode', maxCount: 1 },
+  ])
 )
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE ')
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-	next()
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE '
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
 })
 
 app.use(helmet())
@@ -71,26 +76,26 @@ app.use('/api', jobRoutes)
 app.use('/api/download', desktopRoutes)
 
 app.use((error, req, res, next) => {
-	console.log(error)
-	const status = error.statusCode
-	const message = error.message
-	const data = error.data
-	res.status(status).json({
-		message,
-		data,
-	})
+  console.log(error)
+  const status = error.statusCode
+  const message = error.message
+  const data = error.data
+  res.status(status).json({
+    message,
+    data,
+  })
 })
 
 mongoose
-	.connect(MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(result => {
-		app.listen(process.env.PORT || 5000)
-		console.log(`[+] Connected to Database`)
-		console.log(`Listening on port ${process.env.PORT || 5000}`)
-	})
-	.catch(err => {
-		console.log(err)
-	})
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(result => {
+    app.listen(process.env.PORT || 5000)
+    console.log(`[+] Connected to Database`)
+    console.log(`Listening on port ${process.env.PORT || 5000}`)
+  })
+  .catch(err => {
+    console.log(err)
+  })
